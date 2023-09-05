40bb0d1c528a4f4cafd08d4911e17e4bfa119724
Safat99 <sarkersafat99@gmail.com>
2023-09-05 16:16:57 +0600

    feign proxy client used for communication between two microService

--- order-service/src/main/java/com/microservice_first/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microservice_first/orderservice/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.microservice_first.orderservice.controller;
 
 import com.microservice_first.orderservice.payload.response.UserInfoDto;
+import com.microservice_first.orderservice.proxy.UserServiceProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,25 +17,40 @@ import java.util.HashMap;
 @RestController
 public class OrderController {
 
+    private final UserServiceProxy proxy;
+
+    @Autowired
+    public OrderController(UserServiceProxy proxy) {
+        this.proxy = proxy;
+    }
+
     @GetMapping("/ping")
     public String hello(){
         return "pong";
     }
 
-    @GetMapping("/user-info/{id}")
-    public ResponseEntity<UserInfoDto> getUserInfo(@PathVariable("id") Long userId) {
+//    @GetMapping("/user-info/{id}")
+//    public ResponseEntity<UserInfoDto> getUserInfo(@PathVariable("id") Long userId) {
+//
+//        /*
+//        * hashmap will be used if I have some more path variables and we then bind them in a key-value pair and
+//            pass that as a object.
+//        *
+//        * //HashMap<String, Long> uriVariables = new HashMap<>();
+//        * //uriVariables.put("id", userId);
+//        */
+//
+//        ResponseEntity<UserInfoDto> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/user/{id}", UserInfoDto.class, userId);
+//        UserInfoDto userInfoDto = responseEntity.getBody();
+//
+//        return new ResponseEntity<UserInfoDto>(userInfoDto, HttpStatus.OK);
+//    }
 
-        /*
-        * hashmap will be used if I have some more path variables and we then bind them in a key-value pair and
-            pass that as a object.
-        *
-        * //HashMap<String, Long> uriVariables = new HashMap<>();
-        * //uriVariables.put("id", userId);
-        */
+    @GetMapping("/user-info-feign/{id}")
+    public ResponseEntity<UserInfoDto> getUserInfoByFeign(@PathVariable("id") Long userId) {
 
-        ResponseEntity<UserInfoDto> responseEntity = new RestTemplate().getForEntity("http://localhost:8080/user/{id}", UserInfoDto.class, userId);
-        UserInfoDto userInfoDto = responseEntity.getBody();
-
-        return new ResponseEntity<UserInfoDto>(userInfoDto, HttpStatus.OK);
+        UserInfoDto userInfo = proxy.getUserInfo(userId);
+        return new ResponseEntity<UserInfoDto>(userInfo, HttpStatus.OK);
     }
+    
 }
---
+++ order-service/src/main/java/com/microservice_first/orderservice/proxy/UserServiceProxy.java
@@ -0,0 +1,13 @@
+package com.microservice_first.orderservice.proxy;
+
+import com.microservice_first.orderservice.payload.response.UserInfoDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "user-service", url="localhost:8080")
+public interface UserServiceProxy { 
+
+    @GetMapping("/user/{id}")
+    public UserInfoDto getUserInfo(@PathVariable("id") Long userId);
+}
