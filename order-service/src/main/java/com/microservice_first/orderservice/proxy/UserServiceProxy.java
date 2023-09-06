98a1c587ad6815e733f64130998b12db3bd13850
Safat99 <sarkersafat99@gmail.com>
2023-09-06 18:12:39 +0600

    eureka naming server added and configured

--- order-service/src/main/java/com/microservice_first/orderservice/proxy/UserServiceProxy.java
+++ order-service/src/main/java/com/microservice_first/orderservice/proxy/UserServiceProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url="localhost:8080")
+//@FeignClient(name = "user-service", url="localhost:8080")
+@FeignClient(name = "user-service")
 public interface UserServiceProxy {
 
     @GetMapping("/user/{id}")
